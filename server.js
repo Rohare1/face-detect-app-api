0c7ff28b5595ac96844b7a5b3a667dedc801f0e2
Ryan o'Hare <ryan.1.ohare@gmail.com>
2020-05-08 16:59:11 +0100

    Refactor Server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
@@ -20,39 +19,24 @@ const database = {
 		{
 			id: '124',
 			name: 'sally',
-			email: 'sally@gmail',
+			email: 'sally@gmail.com',
 			password: 'bananas',
 			entries: 0,
 			joined: new Date()
 		}
 	]
 };
- 
+
 app.get('/', (req, res) => {
 	res.send(database.users);
 });
 
 app.post('/signin', (req, res) => {
-	// From the fetch method on Signin.js on frontend 
 	if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
 		res.json(database.users[0]);
 	} else {
-		res.status(400).json('error logging in');
+		res.status(400).json('error loggin in');
 	}
-});
-
-app.post('/register', (req, res) => {
-	// From the fetch method on Register.js on frontend 
-	const { email, name, password } = req.body;
-	database.users.push({
-		id: '125',
-		name: name,
-    email: email,
-    password: password,
-		entries: 0,
-		joined: new Date()
-	});
-	res.json(database.users[database.users.length - 1]);
 });
 
 app.get('/profile/:id', (req, res) => {
@@ -63,35 +47,42 @@ app.get('/profile/:id', (req, res) => {
 			found = true;
 			return res.json(user);
 		}
-	});
-	if (!found) {
-		res.status(404).json('not found');
+	})
+	if (!found){
+		res.status(400).json('not found');
 	}
+});
+
+app.post('/register', (req, res) => {
+	const { email, name, password } = req.body;
+	database.users.push({
+		id: '125',
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	});
+	res.json(database.users[database.users.length - 1]);
 });
 
 app.put('/image', (req, res) => {
 	const { id } = req.body;
 	let found = false;
-	database.users.forEach((user) => {
+	database.users.forEach(user => {
 		if (user.id === id) {
 			found = true;
 			user.entries++;
 			return res.json(user.entries);
 		}
-	});
-	if (!found) {
-		res.status(400).json('not user found');
+	})
+	if (!found){
+		res.status(400).json('not found');
 	}
-});
-
-// // Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//   // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//   // res = false
-// });
+})
 
 app.listen(3000, () => {
-	console.log('The server is running on port 3000');
+	console.log('App is running on port 3000');
 });
+
+
