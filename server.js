b717b4f52b77950c85074d39f310ba882196a0d6
Ryan o'Hare <ryan.1.ohare@gmail.com>
2020-05-12 17:52:22 +0100

    Connect image endpoint to database

--- server.js
+++ server.js
@@ -52,15 +52,18 @@ app.post('/signin', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
-	db.select('*').from('users').where({ id })
+	db
+		.select('*')
+		.from('users')
+		.where({ id })
 		.then((user) => {
-			if(user.length){
-				res.json(user[0])
-			}else{
-				res.status(400).json('Not Found')
+			if (user.length) {
+				res.json(user[0]);
+			} else {
+				res.status(400).json('Not Found');
 			}
 		})
-		.catch(err => res.status(400).json('error geting user'))
+		.catch((err) => res.status(400).json('error geting user'));
 });
 
 app.post('/register', (req, res) => {
@@ -80,17 +83,14 @@ app.post('/register', (req, res) => {
 
 app.put('/image', (req, res) => {
 	const { id } = req.body;
-	let found = false;
-	database.users.forEach((user) => {
-		if (user.id === id) {
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
-	});
-	if (!found) {
-		res.status(400).json('not found');
-	}
+	db('users')
+		.where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then((entries) => {
+			res.json(entries[0]);
+		})
+		.catch((err) => res.status(400).json('unable to get entries'));
 });
 
 app.listen(3000, () => {
