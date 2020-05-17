d62dc426df5406ab89adc0df013a89030801fbdb
Ryan o'Hare <ryan.1.ohare@gmail.com>
2020-05-17 17:16:46 +0100

    Install Clarifai and configure API key

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+	apiKey: '2ed676e6f0174028bf48b1c7d9f7a71d'
+});
+
+
+const handleApiCall = (req, res) => {
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  .then(data => {
+    res.json(data);
+  })
+  .catch(err => res.status(400).json('Unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
 	const { id } = req.body;
 	db('users')
@@ -11,5 +26,5 @@ const handleImage = (req, res, db) => {
 };
 
 module.exports = {
-	handleImage
+	handleImage, handleApiCall
 };
