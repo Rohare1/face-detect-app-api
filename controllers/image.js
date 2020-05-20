f20104c41876fa2ad3e02ea57b2b3de6c01db3ab
Ryan o'Hare <ryan.1.ohare@gmail.com>
2020-05-20 14:45:00 +0100

    refactor handleApiCall

--- controllers/image.js
+++ controllers/image.js
@@ -4,14 +4,14 @@ const app = new Clarifai.App({
 	apiKey: '2ed676e6f0174028bf48b1c7d9f7a71d'
 });
 
-
 const handleApiCall = (req, res) => {
-  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-  .then(data => {
-    res.json(data);
-  })
-  .catch(err => res.status(400).json('Unable to work with API'))
-}
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then((data) => {
+			res.json(data);
+		})
+		.catch((err) => res.status(400).json('Unable to work with API'));
+};
 
 const handleImage = (req, res, db) => {
 	const { id } = req.body;
@@ -26,5 +26,6 @@ const handleImage = (req, res, db) => {
 };
 
 module.exports = {
-	handleImage, handleApiCall
+	handleImage,
+	handleApiCall
 };
